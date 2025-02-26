120c110f757ea6545c5f882470515942c4785e21
Yann <metalyanni@gmail.com>
2025-02-25 21:52:01 -0600

    Creating POST method in controller that return response in JSON format

--- Seccion2/accounts/src/main/java/com/yannbytes/accounts/controller/AccountsController.java
+++ Seccion2/accounts/src/main/java/com/yannbytes/accounts/controller/AccountsController.java
@@ -1,15 +1,23 @@
 package com.yannbytes.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yannbytes.accounts.constants.AccountsConstants;
+import com.yannbytes.accounts.dto.CustomerDto;
+import com.yannbytes.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 //Below annotation is used to mark this class as a controller
 @RestController
+@RequestMapping(path = "/api", produces = (MediaType.APPLICATION_JSON_VALUE))
 public class AccountsController {
 
-    //Below annotation is used to mark this method as a rest endpoint
-    @GetMapping("sayHello")
-    public String sayHello(){
-        return "Hello world";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 }
--- Seccion2/accounts/src/main/java/com/yannbytes/accounts/dto/AccountsDto.java
+++ Seccion2/accounts/src/main/java/com/yannbytes/accounts/dto/AccountsDto.java
@@ -9,5 +9,5 @@ public class AccountsDto {
     private Long accountNumber;
     private String accountType;
     private String branchAddress;
-    
+
 }
