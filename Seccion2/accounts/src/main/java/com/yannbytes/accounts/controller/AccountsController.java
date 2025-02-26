1562649f2556b81e7f9b73cf7df4c0ab82bf8a64
Yann <metalyanni@gmail.com>
2025-02-25 22:34:09 -0600

    Adding the iAccountService in the controller to save in the DB the record comming from the REST method

--- Seccion2/accounts/src/main/java/com/yannbytes/accounts/controller/AccountsController.java
+++ Seccion2/accounts/src/main/java/com/yannbytes/accounts/controller/AccountsController.java
@@ -3,6 +3,8 @@ package com.yannbytes.accounts.controller;
 import com.yannbytes.accounts.constants.AccountsConstants;
 import com.yannbytes.accounts.dto.CustomerDto;
 import com.yannbytes.accounts.dto.ResponseDto;
+import com.yannbytes.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,16 @@ import org.springframework.web.bind.annotation.*;
 //Below annotation is used to mark this class as a controller
 @RestController
 @RequestMapping(path = "/api", produces = (MediaType.APPLICATION_JSON_VALUE))
+@AllArgsConstructor
 public class AccountsController {
+
+    //When we have only one constructor @Autowired annotation is not required
+    private IAccountService iAccountService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
 
+        iAccountService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
