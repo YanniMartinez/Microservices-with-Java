42ba5ffa1243f3d6fe0f4a4fed262591e05735ea
Yann <metalyanni@gmail.com>
2025-03-01 20:10:19 -0600

    Adding documentation in the root app with title and so more

--- Seccion2/accounts/src/main/java/com/yannbytes/accounts/AccountsApplication.java
+++ Seccion2/accounts/src/main/java/com/yannbytes/accounts/AccountsApplication.java
@@ -1,5 +1,10 @@
 package com.yannbytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +12,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 //Se manda el nombre del componente creado en el auditAware
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl" ) //Permite habilitar la auditoria de EntityListeners
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "yann",
+						email = "yann@example.com",
+						url = "https://www.linkedin.com/in/yanni-martinez-martinez/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.linkedin.com/in/yanni-martinez-martinez/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
